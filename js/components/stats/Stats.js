b61b59a5b815976fb196d92b66c2b80e98a42874
Gorm Casper <g@g81.dk>
2016-05-29 22:31:55 +0200

    slkjf

--- js/components/stats/Stats.js
+++ js/components/stats/Stats.js
@@ -3,7 +3,10 @@ import {connect} from 'react-redux';
 import Header from '../header/Header';
 import SummaryTable from './SummaryTable';
 import CategoryStats from './CategoryStats';
-
+import {isRead, bookInfo, bookId} from '../../util/book';
+import filter from '../../util/filter';
+import sorted from '../../util/sorted';
+import to from '../../util/to';
 
 class Stats extends React.Component {
   render () {
@@ -15,6 +18,16 @@ class Stats extends React.Component {
         <SummaryTable />
         <hr />
         <CategoryStats />
+        <hr />
+        <ol reversed={true}>
+          {this.props.books::filter(isRead)::sorted(b => -new Date(b.finished))::to(Array).map(book => {
+            const info = bookInfo(book);
+            const abandoned = book.abandoned
+              ? `(abandoned after ~${book.abandoned} pages)`
+              : null;
+            return <li key={bookId(book)}>{info.title} - {info.authors.join(', ')} {abandoned}</li>;
+          })}
+        </ol>
       </div>
     </div>);
   }
